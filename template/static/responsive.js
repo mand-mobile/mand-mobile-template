94471398ca8c269a229b6d01c58966bc1489631d
xuxiaoyan <xuxiaoyan@didichuxing.com>
2018-11-01 15:29:09 +0800

    fix:responsive initialize synchronization

--- template/static/responsive.js
+++ template/static/responsive.js
@@ -1,27 +1,17 @@
 (function (window, document) {
-
-    function resize(){
-      var ww = window.innerWidth;
-      if (ww > window.screen.width) {
-        window.requestAnimationFrame(resize);
+  function resize () {
+    var ww = window.innerWidth
+    if (ww > window.screen.width) {
+      window.requestAnimationFrame(resize)
+    } else {
+      if (ww > 750) {
+        ww = 750
       }
-      else{
-        if (ww > 720) {
-          ww = 720
-        }
-        // set 1rem based on iPhone6 750px width, 750/7.5=100
-        document.documentElement.style.fontSize = ww / 7.5 + 'px';
-        document.body.style.opacity = 1;
-      }
+      document.documentElement.style.fontSize = ww * 100 / 750 + 'px'
     }
+  }
 
-    if (document.readyState !== 'loading') {
-      resize();
-    }
-    else {
-      document.addEventListener('DOMContentLoaded', resize);
-    }
+  resize()
 
-    window.addEventListener('resize', resize);
-
-  })(window, document)
+  window.addEventListener('resize', resize)
+})(window, document)
